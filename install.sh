8bb374aa4b06adef62fe2b9a892a7c8cc3cf83f6
Insik-Won <insikwon@naver.com>
2022-03-23 03:53:51 +0900

    change relative paths to absolute ones in install.sh by using pwd

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-ln -s ./src/daily_apt_update.sh /usr/local/bin/daily_apt_update
-ln -s ./src/remove_identifier.sh /usr/local/bin/remove_identifier
+ln -sf "$(pwd)/src/daily_apt_update.sh" /usr/local/bin/daily_apt_update
+ln -sf "$(pwd)/src/remove_identifier.sh" /usr/local/bin/remove_identifier
